006957824ac385f8d48b684e96c25ccdb24b44d7
hiraekram10 <heraiqram123@gmail.com>
2024-02-24 22:32:02 +0500

    updated

--- step00_intro/index.js
+++ step00_intro/index.js
@@ -1,30 +1,13 @@
 // 00 hello  world
-
 // let fullName = "Hira ekram"
 // console.log(fullName);
-
-
 // 00b  syntax error
-
 // let fName = "hira"
 // consule.log(fName) // error TS2552: Cannot find name 'consule'. Did you mean 'console'?
-
-
 //00c type error 
 // letter user ="Teacher";
 // console.log(user); // Unknown keyword or identifier. Did you mean 'let ter'?
-
-
 // 00d assignability error
-
-
-// let admin = 0;
-// admin = "safia"
-// console.log(admin);   //Type 'string' is not assignable to type 'number'.
-
-
-
-
-
-
-
+var admin = 0;
+admin = "safia";
+console.log(admin);
