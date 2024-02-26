9dd56481d10e71f4b0267ad03be242cf2e474426
hiraekram10 <heraiqram123@gmail.com>
2024-02-26 22:01:13 +0500

    union

--- step04_union_literals/index.js
+++ step04_union_literals/index.js
@@ -1,5 +1,11 @@
-//
-var myClass;
-myClass = null; //error got to change in ts config change null check true
-myClass = null; //no error
-console.log(myClass);
+//  error solving
+//  let myClass :number;
+//  myClass = null  //error got to change in ts config change null check true
+//  myClass = null //no error
+//  console.log(myClass);
+//Union 
+//The first way to combine types you might see is a union type
+var food;
+food = "Biryani";
+food.toLocaleLowerCase();
+console.log(food); //type str
--- step04_union_literals/index.ts
+++ step04_union_literals/index.ts
@@ -6,5 +6,51 @@
 
 //  myClass = null //no error
 //  console.log(myClass);
+
+
+//Union 
+//The first way to combine types you might see is a union type
+let food: string | number;
+
+//he process of knowing the type of a variable is known as type narrowing
+food = "Biryani";  // narowwing
+
+console.log(food); //type str
+
+// food.tofixed()  //  Property 'tofixed' does not exist on type 'string'. Did you mean 'fixed
+
+
+
+let Id = Math.random() <0.5 ? "greater" :1;
+
+// if(Id == "greater"){
+//     Id.toLowerCase();
+
+// }else{
+//   Id.upperCase() //Property 'upperCase' does not exist on type 'string | number'.
+// }
+
+
+// narrow down
+// In TypeScript, type narrowing is the removal of types from a union. 
+// To narrow a variable to a specific type, we use the type guard. We can use the typeof operator with the 
+// variable name and compare it with the type that you expect for the variable.
+
+
+
+// if( typeof Id === "string" ){
+//     Id.length
+// }else{
+//     typeof Id  //no
+
+// }
+
+//  typeof Id === "string"
+//? Id.length 
+// :Id.toFixed();
+
+
+
+
  
  
