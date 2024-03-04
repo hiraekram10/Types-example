a0d0872177b7c2c1725d775e1b08adaa01fde703
hiraekram10 <heraiqram123@gmail.com>
2024-03-05 00:03:08 +0500

    changes occur

--- step001_var_arr_obj/main.js
+++ step001_var_arr_obj/main.js
@@ -1,2 +1,15 @@
 var work = "hi";
 console.log(work);
+
+//variable
+//a named space in the memory
+//it acts as a container for values in a program
+//allowed characters _,$
+//cannot begin with digit (2no)
+// a variable should declared before used
+//  variable,something which is changable(ek dabba var uska name or asingment operator value 320 )
+
+
+let firstname = "hira";
+let Firstname  = "sana"
+
