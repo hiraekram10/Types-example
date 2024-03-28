b7ce3910707e751b2a66ae0634067ad30dc5a1e1
hiraekram10 <heraiqram123@gmail.com>
2024-03-29 00:00:21 +0500

    changed

--- step_operators/main.js
+++ step_operators/main.js
@@ -5,25 +5,15 @@
 // const ricePrice:number = 100;
 // const pulsesPrice:number = 900;
 // const flourPrice:number = 200;
-
 // totalAmount = oilPrice + ricePrice + pulsesPrice + flourPrice
 // console.log("your total amount is: ",totalAmount);
-
-
 // let mysavePass = 896734;
 // let addingPass = 896734;
 // console.log(mysavePass === addingPass);
-
-
-let isclassesOpen = false;
-
-if(isclassesOpen){
+var isclassesOpen = false;
+if (isclassesOpen) {
     console.log('sahi bat hai');
-    
-}else{
-    console.log('ghlt bat');
-    
 }
-
-
-
+else {
+    console.log('ghlt bat');
+}
